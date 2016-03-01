f9f2516bba854d05c4b5672065f6620bf9ce337f
ChrisLane <ChrisLane@users.noreply.github.com>
2016-03-01 23:30:35 +0000

    Comment code for submission

--- src/evalobj.cpp
+++ src/evalobj.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 #include "evalobj.h"
 
+/**
+ * Handle evaluation of let expressions.
+ * Add the variable for the let and then evaluate the expression with this new value.
+ */
 int Let::eval(env *environment) {
     env *newEnvironment = new env();
     newEnvironment->var = bvar;
@@ -13,6 +17,10 @@ int Let::eval(env *environment) {
     return body->eval(newEnvironment);
 }
 
+/**
+ * Handle evaluation of operators.
+ * If the operator is plus then add the variables, if it's times then multiply them.
+ */
 int OpApp::eval(env *environment) {
     switch (op) {
         case plusop: {
@@ -37,6 +45,10 @@ int OpApp::eval(env *environment) {
     return 0;
 }
 
+/**
+ * Handle evaluation of variables.
+ * Return the value of the variable.
+ */
 int Var::eval(env *environment) {
     while (environment) {
         if (name.compare(environment->var) == 0) {
@@ -48,6 +60,10 @@ int Var::eval(env *environment) {
     return 0;
 }
 
+/**
+ * Handle evaluation of constants.
+ * Return the constant.
+ */
 int Constant::eval(env *) {
     return n;
 }
